5cee74f48c60df42929aa1ed768f1bb50e5009d9
arturogirona@outlook.com <arturogirona@localhost.localdomain>
2021-09-30 20:08:41 -0400

    First Commit!

---
+++ launcher.py
@@ -0,0 +1,18 @@
+import hikari
+from hikari import Command
+
+bot = hikari.GatewayBot(token="")
+
+
+@bot.listen()
+async def ping(event: hikari.MessageCreateEvent) -> None:
+    # If a non-bot user sends a message "hk.ping", respond with "Pong!"
+    # We check there is actually content first, if no message content exists,
+    # we would get `None' here.
+    if event.is_bot or not event.content:
+        return
+
+    if event.content.startswith("hk.ping"):
+        await event.message.respond("Pong!")
+
+bot.run()
