850f3f9d3bcc32593c7939d7dd7e1dee2b37e64a
bennguyen334 <81700307+bennguyen334@users.noreply.github.com>
2021-10-05 21:09:32 -0400

    10/5/21
    ben's test bot, finished video 1 on tutorial

---
+++ testbot/bot/extensions/admin.py
@@ -0,0 +1,19 @@
+import lightbulb
+
+from testbot.bot import Bot
+
+
+class Admin(lightbulb.Plugin):
+    @lightbulb.check(lightbulb.owner_only)
+    @lightbulb.command(name="shutdown", aliases=("sd",))
+    async def command_shutdown(self, ctx: lightbulb.Context) -> None:
+        await ctx.bot.close()
+
+
+
+def load(bot: Bot) -> None:
+    bot.add_plugin(Admin())
+
+
+def unload(bot: Bot) -> None:
+    bot.remove_plugin("Admin")
