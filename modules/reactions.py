ad289f54bc24f4e7f81b1605ac7ed04d6586b221
Arturo Girona <arturogirona@outlook.com>
2021-11-24 14:43:27 -0500

    Played with Reactions a bit

---
+++ modules/reactions.py
@@ -0,0 +1,7 @@
+import hikari
+import lightbulb
+import typing
+from lightbulb import slash_commands
+
+class Makereaction(lightbulb.SlashCommand):
+    description = "Posts a rection under this message"
